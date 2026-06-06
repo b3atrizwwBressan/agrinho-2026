716d7fada7628d51bded5f456643ccf06c5016d1
b3atrizwwBressan <Beatriz.bressan.torres@escola.pr.gov.br>
2026-06-06 10:55:40 -0300

    eeeee

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
 // Seleciona o botão e o áudio do HTML
 const botaoPlay = document.getElementById('btn-play');
 const musica = document.getElementById('musica-jogo');
+// Seleciona também o game-board para podermos mudar o fundo dele
+const gameBoard = document.querySelector('.game-board');
 
 // Adiciona o evento de clique no botão
 botaoPlay.addEventListener('click', function() {
     musica.play();
     console.log("A música começou a tocar!");
+
+    // 1. Faz a tela escurecer adicionando a classe do CSS
+    gameBoard.classList.add('escurecer');
+
+    // 2. Espera 500 milissegundos (o tempo do efeito de fechar a tela)
+    setTimeout(function() {
+        
+        // 3. Troca o cenário de fundo e esconde o botão de Play
+        gameBoard.classList.add('cenario2');
+        botaoPlay.style.display = 'none'; 
+
+        // 4. Remove o efeito preto para o novo cenário aparecer
+        gameBoard.classList.remove('escurecer');
+
+    }, 500);
 });
